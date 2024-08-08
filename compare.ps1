2245109c7148a18be9a745b25e4423d84da3a871
Drako-PxPx <drako@paxpax.me>
2024-08-08 13:01:42 -0600

    Added append fields feature

--- compare.ps1
+++ compare.ps1
@@ -14,10 +14,31 @@ Write-Host $myJson.branchName
 
 foreach ($i in $myJson.transitions) {
   # Write-Host $i.description
-  $d1path = Join-Path $git_path $i.source
+  if ($i.source) {
+    $d1path = Join-Path $git_path $i.source
+  } else {
+    $d1path = $null
+  }
   $d2path = Join-Path $git_path $i.target
-  $d1 = [datetime](Get-ItemProperty -Path $d1path -Name LastWriteTime).LastWriteTime
-  $d2 = [datetime](Get-ItemProperty -Path $d2path -Name LastWriteTime).LastWriteTime
+  
+  try {
+    $d1 = [datetime](Get-ItemProperty -Path $d1path -Name LastWriteTime -ErrorAction Stop).LastWriteTime
+  }
+  catch {
+    # No destination file 
+    $d1 = [datetime](Get-ItemProperty -Path $project_path -Name LastWriteTime).LastWriteTime
+  }
+  
+
+  try {
+    $d2 = [datetime](Get-ItemProperty -Path $d2path -Name LastWriteTime -ErrorAction Stop).LastWriteTime
+  }
+  catch {
+    # No destination file 
+    $d2 = [datetime](Get-Date -Date "01-01-1970" )
+  }
+
+
 
   if ($d1 -gt $d2) {
     Write-Host $i.description
@@ -26,9 +47,19 @@ foreach ($i in $myJson.transitions) {
       Copy {
         Copy-Item $d1path -Destination $d2path
       }
-      template-copy {
-        m4 -D __TABLE_NAME__="$($i.table_name)" -D __TABLE_OWNER__="$($i.table_owner)" -D __KEYSEQUENCE__="$($i.PKSEQ)" -D __PK__="$($i.PK)" -D __TABLEDEF__="$($d1path)" (Join-Path $PSScriptRoot "/templates/$($i.template)") > $d2path
+      table-template-copy {
+        m4 -D __TABLE_NAME__="$($i.table_name)" -D __TABLE_OWNER__="$($i.table_owner)" -D __KEYSEQUENCE__="$($i.PKSEQ)" -D __PK__="$($i.PK)" -D __TABLEDEF__="$($d1path)" (Join-Path $PSScriptRoot "/templates/newtable.m4") > $d2path
       }
+      rollout-fields {
+        $fieldlist = ""
+        $typeslist = ""
+        foreach ($j in $i.fields) {
+          $fieldlist = "$($fieldlist)$($j.name);"
+          $typeslist = "$($typeslist)$($j.type);"
+        }
+        m4 -D __TABLE_NAME__="$($i.table_name)" -D __TABLE_OWNER__="$($i.table_owner)" -D __FIELDS_LIST__="$($fieldlist)" -D __DATA_TYPES__="$($typeslist)" (Join-Path $PSScriptRoot "/templates/appendfields.m4") > $d2path
+      }
+
     }
   }
 }
