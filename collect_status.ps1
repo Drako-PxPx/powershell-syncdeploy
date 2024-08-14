b0607f78fc98f222e9f7870652cfde93777964b8
Drako-PxPx <drako@paxpax.me>
2024-08-14 14:06:45 -0600

    Collect all information on a single call

--- collect_status.ps1
+++ collect_status.ps1
@@ -20,24 +20,7 @@ $env:SQLPATH = "sql"
 
 $stamp = $(Get-Date).ToString("yyyyMMddHHmm");
 
-New-Item -Path "_output" -Name $stamp -ItemType Directory
-$job_count=0
-$owners = $(sqlcmd -script list_owners).split(';')
-foreach ($owner in $owners) {
-  if ($owner) {
-    foreach ($objecttype in $object_types) {
-      $output_file = "_output/$($stamp)/$($owner)_$($objecttype).json"
-      # Write-Host $output_file
-      $job_count = $job_count + 1
-      $pc = ($job_count / ($owners.count * $object_types.count))*100
-      Write-Progress -Activity "Collecting current object statuses" -PercentComplete $pc -CurrentOperation "Schema: $($owner), Set: $($objecttype)"
-      try {
-        sqlcmd -script object_statuses -par1 $owner -par2 $objecttype |  ConvertFrom-Json -ErrorAction stop | ConvertTo-Json | Out-File -FilePath $output_file
-      } catch
-      {
-        Write-Host "Schema: $($owner), Set: $($objecttype)"
-      }
 
-    }
-  }
-}
+$output_file = "_output/$($stamp)_$($tnsstring).json"
+sqlcmd -script object_statuses |  ConvertFrom-Json -ErrorAction stop | ConvertTo-Json | Out-File -FilePath $output_file
+
--- sql/object_statuses.sql
+++ sql/object_statuses.sql
@@ -1,23 +1,13 @@
 set ver off pages 999 heading off feedback off
 prompt [
 select json_object
-  ( key 'S'  value decode(status,'VALID','V','INVALID','I'),
-    key 'OT' value decode(object_type, 'SYNONYM',      'SN',
-                                       'PACKAGE BODY', 'PB',
-                                       'TYPE BODY',    'TB',
-                                       'TRIGGER',      'TG',
-                                       'PACKAGE',      'PK',
-                                       'PROCEDURE',    'PC',
-                                       'FUNCTION',     'FC',
-                                       'TYPE',         'TP',
-                                       'MATERIALIZED VIEW','MV',
-                                       'VIEW', 'VW'
-                         ),
-    key 'ON' value object_name
+  ( key 'status'  value status,
+    key 'object-type' value object_type,
+    key 'object-name' value object_name,
+    key 'owner' value owner
   ) || ','
-  from dba_objects
- where owner = '&1.'
-   and object_type = '&2.'
- order by object_name;
+  from all_objects  
+ where object_type in ('SYNONYM','PACKAGE BODY','TYPE BODY','TRIGGER','PACKAGE','PROCEDURE','FUNCTION','TYPE','MATERIALIZED VIEW','VIEW')
+ order by owner, object_name;
 prompt {}]
 exit;
